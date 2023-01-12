d335ee2e220b727e8155925f58a85303386f982f
coldsnap90 <cfarbatuk@gmail.com>
2023-01-11 18:11:41 -0800

    heroku

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from park import create_app,os
 app = create_app()
 if __name__ == '__main__'    :  
    #port = int(os.environ.get('PORT', 5000))                                                             
-   app.)
+   app.run()
