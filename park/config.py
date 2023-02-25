d76acde197d1b797df49ec9c449e764d7b04bfc9
coldsnap90 <cfarbatuk@gmail.com>
2023-02-24 20:50:49 -0800

    jobs

--- park/config.py
+++ park/config.py
@@ -15,7 +15,7 @@ class Config:
     SERVER_NAME = "127.0.0.1:5000"
     SERVER_NAME = "webookcamp.herokuapp.com"
     #"sqlite:///db.job"
-    SCHEDULER_JOBSTORES = {"default": SQLAlchemyJobStore(url='postgres://xqoywsixdkorla:ac760ec6200df59ac4e33e149832a5f7664f3f78de429f22c609722fb9eedf5b@ec2-54-157-79-121.compute-1.amazonaws.com:5432/d9ilpqshnfmtq3')}
+    SCHEDULER_JOBSTORES = {"default": SQLAlchemyJobStore(url='postgresql://xqoywsixdkorla:ac760ec6200df59ac4e33e149832a5f7664f3f78de429f22c609722fb9eedf5b@ec2-54-157-79-121.compute-1.amazonaws.com:5432/d9ilpqshnfmtq3')}
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///db.database'
     SECRET_KEY = os.environ.get('ENV_SECRET_KEY')
     #SECURITY_PASSWORD_SALT = 'SUPER SALTY STRING !@#$%^&*()'
