5d802ce3aabcc747e3f42aeba2103eccc811578e
coldsnap90 <cfarbatuk@gmail.com>
2023-03-21 21:01:34 -0700

    mp

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from park import create_app,os
 app = create_app()
 if __name__ == '__main__'    :  
    port = int(os.environ.get('PORT', 5000))                                                                    
-   app.run(debug = True)
+   app.run()
